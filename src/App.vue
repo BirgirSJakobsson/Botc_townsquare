f4c28b576b6112918bd6e9c829e059c07955f1d5
BirgirSJakobsson <birgirsjakobsson@gmail.com>
2024-02-21 19:05:25 +0000

    Revert "double check"
    This reverts commit 81e2bddad32e5eeb2b096f973460c07c7eb34908.

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,15 @@
 <template>
-  <div id="app">
-    <h1>Vue.js Socket.io Chat</h1>
-    <input v-model="message" @keyup.enter="sendMessage">
-    <button @click="sendMessage">Send</button>
-    <ul>
-      <li v-for="msg in messages" :key="msg">{{ msg }}</li>
-    </ul>
+  <div class="Main">
+    <controls></controls>
+    <v-header></v-header>
+    <v-body></v-body>
+    <v-footer></v-footer>
   </div>
 </template>
 
 <script>
-  export default{
-    data(){
-      return{
-        message: '',
-        messages: []
-      };
-    },
-    sockets:{
-      connect(){
-        console.log('Connected to Server');
-      },
-      disconnect(){
-        console.log('Disconnected from Server');
-      },
-      'chat message'(msg){
-        this.messages.push(msg);
-      }
-    },
-    methods:{
-      sendMessage(){
-        if(this.message.trim() !== ''){
-          this.$socket.emit('chat message', this.message);
-          this.message = '';
-        }
-      }
-    }
-  };
-</script>
-
-<!--<controls></controls>
-    <v-header></v-header>
-    <v-body></v-body>
-    <v-footer></v-footer>-->
-
-    <!--import Controls from './components/Controls.vue'
+  
+  import Controls from './components/Controls.vue'
   import Header from './components/Header.vue'
   export default {
     name : 'App',
@@ -52,4 +17,5 @@
       'controls' : Controls,
       'v-header' : Header,
     }
-  }-->
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
 import './assets/main.css'
 
-import {createApp} from 'vue';
-import App from './App.vue';
-import VueSocketIO from 'vue-socket.io';
-
-Vue.config.productionTip = false;
-
-Vue.use(new VueSocketIO({
-  connection: 'http://localhost:3000'
-}));
+import { createApp } from 'vue'
+import App from './App.vue'
 
 const app = createApp(App)
 app.mount('#app')
