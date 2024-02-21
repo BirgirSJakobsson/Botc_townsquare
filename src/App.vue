32f1c183c90dd355f40219c7d7cc8c01d3284720
BirgirSJakobsson <birgirsjakobsson@gmail.com>
2024-02-21 18:43:37 +0000

    First commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="Main">
+    <controls></controls>
+    <v-header></v-header>
+    <v-body></v-body>
+    <v-footer></v-footer>
+  </div>
+</template>
+
+<script>
+  
+  import Controls from './components/Controls.vue'
+  import Header from './components/Header.vue'
+  export default {
+    name : 'App',
+    components : {
+      'controls' : Controls,
+      'v-header' : Header,
+    }
+  }
+</script>
---
+++ src/components/Controls.vue
@@ -0,0 +1,11 @@
+<template>
+<H2>Navbar</H2>
+<div>
+    
+</div>
+</template>
+<script>
+export default{
+    name : 'Controls',
+}
+</script>
