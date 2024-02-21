81e2bddad32e5eeb2b096f973460c07c7eb34908
BirgirSJakobsson <birgirsjakobsson@gmail.com>
2024-02-21 19:03:44 +0000

    double check

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,50 @@
 <template>
-  <div class="Main">
-    <controls></controls>
-    <v-header></v-header>
-    <v-body></v-body>
-    <v-footer></v-footer>
+  <div id="app">
+    <h1>Vue.js Socket.io Chat</h1>
+    <input v-model="message" @keyup.enter="sendMessage">
+    <button @click="sendMessage">Send</button>
+    <ul>
+      <li v-for="msg in messages" :key="msg">{{ msg }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-  
-  import Controls from './components/Controls.vue'
+  export default{
+    data(){
+      return{
+        message: '',
+        messages: []
+      };
+    },
+    sockets:{
+      connect(){
+        console.log('Connected to Server');
+      },
+      disconnect(){
+        console.log('Disconnected from Server');
+      },
+      'chat message'(msg){
+        this.messages.push(msg);
+      }
+    },
+    methods:{
+      sendMessage(){
+        if(this.message.trim() !== ''){
+          this.$socket.emit('chat message', this.message);
+          this.message = '';
+        }
+      }
+    }
+  };
+</script>
+
+<!--<controls></controls>
+    <v-header></v-header>
+    <v-body></v-body>
+    <v-footer></v-footer>-->
+
+    <!--import Controls from './components/Controls.vue'
   import Header from './components/Header.vue'
   export default {
     name : 'App',
@@ -17,5 +52,4 @@
       'controls' : Controls,
       'v-header' : Header,
     }
-  }
-</script>
+  }-->
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue';
+import App from './App.vue';
+import VueSocketIO from 'vue-socket.io';
+
+Vue.config.productionTip = false;
+
+Vue.use(new VueSocketIO({
+  connection: 'http://localhost:3000'
+}));
 
 const app = createApp(App)
 app.mount('#app')
